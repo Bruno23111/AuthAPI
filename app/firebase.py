55ce246c3273c88ae2dbb47c6d7c8e9f0f517275
Yago da Silva Leme <143760057+YagoL3m3@users.noreply.github.com>
2025-05-21 11:29:38 -0300

    Alteração FIREBASE
    Inserção de comentarios para a melhor interpretação do código

--- app/firebase.py
+++ app/firebase.py
@@ -4,7 +4,10 @@ import os
 
 # Evita inicializar o Firebase mais de uma vez
 if not firebase_admin._apps:
+    # Lê um arquivo .json que contém a chave privada de acesso ao Firebase.
     cred = credentials.Certificate("firebase-credentials.json")
+    # Inicializa a aplicação Firebase
     firebase_admin.initialize_app(cred)
-
+    
+# Inicializa o cliente de banco de dados Firestore, armazenado na variável db
 db = firestore.client()
