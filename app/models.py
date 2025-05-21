62d5b941c1b48fd7e8088dbbce9b981163c8a363
Yago da Silva Leme <143760057+YagoL3m3@users.noreply.github.com>
2025-05-21 10:57:37 -0300

    Alteração no Models
    Inserção de comentarios para melhor interpretação do código

--- app/models.py
+++ app/models.py
@@ -1,14 +1,18 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 
+# modelo definido para criar uma task 
 class TaskCreate(BaseModel):
     title: str
     description: Optional[str] = None
     completed: bool = False
 
+# modelo usado para representar uma tarefa completa
+# nesse modelo especifico a classe Task esta herdando tudo da TaskCreate e esta adicionando o campo creator_id
 class Task(TaskCreate):
     creator_id: str
 
+# modelo usado para criar um novo usuario
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
