2301084ec0cc354fb99b00a7942f22fd47c0a681
Yago da Silva Leme <143760057+YagoL3m3@users.noreply.github.com>
2025-05-21 11:13:30 -0300

    Alteração da MAIN
    Inserção de comentarios para a melhor interpretação do código

--- app/main.py
+++ app/main.py
@@ -5,14 +5,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from .routes import router
 from fastapi.responses import JSONResponse
 
+# aqui [e criada a instancia da API. Todas as configurações são feitas a partir desse objeto app
 app = FastAPI()
 
+# define quais são os dominios que podem fazer requisições nessa API
 origins = [
     "http://localhost:3000",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
 ]
 
+# adicona o middleware Cors a aplicação
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,      
@@ -21,13 +24,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+# incluindo o roteador definido no arquivo routes.py
 app.include_router(router, prefix="/api")
 
+# define uma rota para o caminho raiz e retorna um json
 @app.get("/")
 def root():
     return {"message": "API FastAPI + Firebase está no ar!"}
 
+# esta condição permite que o script seja executado executado diretamente via python main.py
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Pega a porta da variável PORT ou usa 8000 por padrão
     uvicorn.run("main:app", host="0.0.0.0", port=port)
